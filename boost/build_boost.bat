1343d8c3bcff82d30ad9d92d6f0225ac3e935c6f
Ronald <followait@yahoo.com>
2019-04-25 13:34:08 +0800

    update boost build script

--- boost/build_boost.bat
+++ boost/build_boost.bat
@@ -5,6 +5,7 @@ echo Actions:
 echo 0. Quit
 echo 1. VS2015
 echo 2. VS2017
+echo 3. VS2019
 set /p action="Please select an action: "
 
 if %action%  == 0 (
@@ -18,22 +19,28 @@ if %action%  == 0 (
             set toolset=msvc-14.1
             set foldername_for_vs=VS2017
         ) else (
-            echo.
-            goto choose_build_option
+            if %action%==3 (
+				set toolset=msvc
+				set foldername_for_vs=VS2019
+			) else (
+				echo.
+				goto choose_build_option
+			)
         )
     )
 )
 
 rem Directories
-set boost_dir=D:\Dev\Lib\boost\boost_1_68_0
-set build_dir=..\build\%foldername_for_vs%\x64\build
-set stage_dir=..\build\%foldername_for_vs%\x64\stage
-set install_dir=..\dist\%foldername_for_vs%\x64
+set lib_root_dir=D:\dev\lib
+set source_dir=%lib_root_dir%\src\boost_1_70_0
+set build_dir=%lib_root_dir%\build\%foldername_for_vs%\x64\build
+set stage_dir=%lib_root_dir%\build\%foldername_for_vs%\x64\stage
+set install_dir=%lib_root_dir%\dist\boost_1_70_0\%foldername_for_vs%\x64
 
 rem Number of cores for building
 set cores=%NUMBER_OF_PROCESSORS%
 
-echo Source Code Dir: %boost_dir%
+echo Source Dir: %source_dir%
 echo Build Dir: %build_dir%
 echo Stage Dir: %stage_dir%
 echo Install Dir: %install_dir%
@@ -41,7 +48,7 @@ echo Toolset: %toolset%
 echo Cores for building: %cores%
 echo Building Start
 
-cd "%boost_dir%"
+cd "%source_dir%"
 call bootstrap.bat
  
 rem Most libraries can be static libs ???
