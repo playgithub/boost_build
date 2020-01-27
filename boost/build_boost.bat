2640c6859834e23a9442c091601320cac52d4c51
a <a@b.com>
2020-01-27 23:37:51 +0800

    Refine build script for boost

--- boost/build_boost.bat
+++ boost/build_boost.bat
@@ -13,15 +13,15 @@ if %action%  == 0 (
 ) else (
     if %action%==1 (
         set toolset=msvc-14.0
-        set foldername_for_vs=VS2015
+        set folder_name_for_vs=VS2015
     ) else (
         if %action%==2 (
             set toolset=msvc-14.1
-            set foldername_for_vs=VS2017
+            set folder_name_for_vs=VS2017
         ) else (
             if %action%==3 (
 				set toolset=msvc
-				set foldername_for_vs=VS2019
+				set folder_name_for_vs=VS2019
 			) else (
 				echo.
 				goto choose_build_option
@@ -31,27 +31,28 @@ if %action%  == 0 (
 )
 
 rem Directories
-set lib_root_dir=D:\dev\lib
-set source_dir=%lib_root_dir%\src\boost_1_70_0
-set build_dir=%lib_root_dir%\build\%foldername_for_vs%\x64\build
-set stage_dir=%lib_root_dir%\build\%foldername_for_vs%\x64\stage
-set install_dir=%lib_root_dir%\dist\boost_1_70_0\%foldername_for_vs%\x64
+set lib_root_folder_path=C:\Users\disc\Dev\lib
+set boost_folder_name=boost_1_72_0
+set source_folder_path=%lib_root_folder_path%\src\%boost_folder_name%
+set build_folder_path=%lib_root_folder_path%\build\%boost_folder_name%\%folder_name_for_vs%\x64\build
+set stage_folder_path=%lib_root_folder_path%\build\%boost_folder_name%\%folder_name_for_vs%\x64\stage
+set install_folder_path=%lib_root_folder_path%\dist\%boost_folder_name%\%folder_name_for_vs%\x64
 
 rem Number of cores for building
 set cores=%NUMBER_OF_PROCESSORS%
 
-echo Source Dir: %source_dir%
-echo Build Dir: %build_dir%
-echo Stage Dir: %stage_dir%
-echo Install Dir: %install_dir%
+echo Source Folder Path: %source_folder_path%
+echo Build Folder Path: %build_folder_path%
+echo Stage Folder Path: %stage_folder_path%
+echo Install Folder Path: %install_folder_path%
 echo Toolset: %toolset%
 echo Cores for building: %cores%
 echo Building Start
 
-cd "%source_dir%"
+cd "%source_folder_path%"
 call bootstrap.bat
  
 rem Most libraries can be static libs ???
-b2 -j%cores% toolset=%toolset% address-model=64 architecture=x86 threading=multi variant=debug,release link=shared runtime-link=shared install --build-dir="%build_dir%" --stagedir="%stage_dir%" --prefix="%install_dir%"
+b2 -j%cores% toolset=%toolset% address-model=64 architecture=x86 threading=multi variant=debug,release link=shared runtime-link=shared install --build-dir="%build_folder_path%" --stagedir="%stage_folder_path%" --prefix="%install_folder_path%"
 
 pause
